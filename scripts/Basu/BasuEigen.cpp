bc09b3615a4a52483e93e5223a1de00eb7d4c9a2
Ian Bell <ian.bell@nist.gov>
2021-02-09 11:54:37 -0500

    Eigen + Basu working

---
+++ scripts/Basu/BasuEigen.cpp
@@ -0,0 +1,110 @@
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <Eigen/Dense>
+
+template<typename vectype>
+double Clenshaw1D(const vectype &c, double ind){
+    int N = static_cast<int>(c.size()) - 1;
+    double u_k = 0, u_kp1 = 0, u_kp2 = 0;
+    for (int k = N; k >= 0; --k){
+        // Do the recurrent calculation
+        u_k = 2.0*ind*u_kp1 - u_kp2 + c[k];
+        if (k > 0){
+            // Update the values
+            u_kp2 = u_kp1; u_kp1 = u_k;
+        }
+    }
+    return (u_k - u_kp2)/2;
+}
+
+/// With STL datatypes
+template<typename Mat, typename vectype>
+double Clenshaw2D(const Mat& a, double x, double y, vectype& b) {
+    std::size_t m = a.size() - 1;
+    std::size_t n = a[0].size() - 1;
+    for (auto i = 0; i < b.size(); ++i) {
+        b[i] = Clenshaw1D(a[i], y);
+    }
+    return Clenshaw1D(b, x);
+}
+
+template<typename MatType, int Rows = MatType::RowsAtCompileTime, int Cols = MatType::ColsAtCompileTime>
+auto Clenshaw1DByRow(const MatType& c, double ind) {
+    int N = static_cast<int>(c.rows()) - 1;
+    Eigen::Array<double, 1, Cols> u_k, u_kp1, u_kp2; // static so that they are pre-allocated
+    if constexpr (Cols < 0) {
+        u_k.resizeLike(c); 
+        u_kp1.resizeLike(c);
+        u_kp2.resizeLike(c);
+    }
+    u_k.setZero(); u_kp1.setZero(); u_kp2.setZero();
+    
+    for (int k = N; k >= 0; --k) {
+        // Do the recurrent calculation
+        u_k = 2.0 * ind * u_kp1 - u_kp2 + c.row(k);
+        if (k > 0) {
+            // Update the values
+            u_kp2 = u_kp1; u_kp1 = u_k;
+        }
+    }
+    return (u_k - u_kp2) / 2;
+}
+
+/// With Eigen datatypes
+template<typename MatType>
+double Clenshaw2DEigen(const MatType& a, double x, double y) {
+    auto b = Clenshaw1DByRow(a, y);
+    return Clenshaw1D(b.matrix(), x);
+}
+
+int main(){
+    std::vector<std::vector<double>> a;
+    const int M = 8;
+    for (auto i = 0; i <= M; ++i){
+        a.push_back(std::vector<double>(M+1, 0.0)); // One would normally use Eigen here, but the challenge is to use only standard library elements...
+    }
+    {
+        for (auto i = 0; i < M + 1; ++i) {
+            for (auto j = 0; j < M + 1; ++j) {
+                a[i][j] = i + j;
+            }
+        }
+        std::vector<double> b(M+1, 0.0);
+        int N = 1000 * 1000;
+        volatile auto r = 0.0, x = 0.1, y = 0.7;
+        auto startTime = std::chrono::system_clock::now();
+        for (int i = 0; i < N; ++i) {
+            auto v = Clenshaw2D(a, x, y, b);
+            r += v;
+        }
+        auto endTime = std::chrono::system_clock::now();
+        auto elap_us = std::chrono::duration<double>(endTime - startTime).count() / N * 1e6;
+        std::cout << elap_us << " us/call. value:" << (r / N) << " val: " << std::endl;
+    }
+    {
+        constexpr int Rows = M + 1;// Eigen::Dynamic;
+        constexpr int Cols = Eigen::Dynamic;
+        using MatType = Eigen::Array<double, Rows, Cols>;
+        MatType aa; 
+        if constexpr ((Rows < 0) || (Cols < 0)) {
+            aa.resize(M + 1, M + 1);
+        }
+        aa.fill(0.0);
+        for (auto i = 0; i < M + 1; ++i) {
+            for (auto j = 0; j < M + 1; ++j) {
+                aa(i, j) = i + j;
+            }
+        }
+        int N = 1000 * 1000;
+        volatile auto r = 0.0, x = 0.1, y = 0.7;
+        auto startTime = std::chrono::system_clock::now();
+        for (int i = 0; i < N; ++i) {
+            auto v = Clenshaw2DEigen(aa, x, y);
+            r += v;
+        }
+        auto endTime = std::chrono::system_clock::now();
+        auto elap_us = std::chrono::duration<double>(endTime - startTime).count() / N * 1e6;
+        std::cout << elap_us << " us/call. (Eigen-powered) value:" << (r / N) << " val: " << std::endl;
+    }
+}
--- scripts/test_Basu.py
+++ scripts/test_Basu.py
@@ -3,33 +3,52 @@ A driver script to test the implementation in cheb2d_Basu for correctness and sp
 """
 import timeit
 from cheb2d_Basu import *
+import matplotlib.pyplot as plt
+import pandas
 
-x_ = 0.2
-y_ = 0.1
+x_ = 0.7
+y_ = 0.7
 
 # f = lambda x, y: np.sin(x)*np.cos(-0.01*y**2)
 f = lambda x, y: x**3*np.sin(-x**2)*np.cos(x)*np.exp(-x**2-y**2)
 
-a = get_coeff_mat(f, m=10, n=10)
+o = []
+for M in [4,6,8,10,12,14,16]:
+    print(M)
+    a = get_coeff_mat(f, m=M, n=M)
 
-N = 100
+    N = 100
 
-tic = timeit.default_timer()
-for i in range(N):
-    eval_naive(amat=a, x=x_, y=y_)
-toc = timeit.default_timer()
-print((toc-tic)/N*1e6)
+    tic = timeit.default_timer()
+    for i in range(N):
+        eval_naive(amat=a, x=x_, y=y_)
+    toc = timeit.default_timer()
+    time_naive = (toc-tic)/N*1e6
+    print(time_naive, 'us/call for naive implementation')
 
-tic = timeit.default_timer()
-for i in range(N):
-    eval_Clenshaw(amat=a, x=x_, y=y_)
-toc = timeit.default_timer()
-print((toc-tic)/N*1e6)
+    tic = timeit.default_timer()
+    for i in range(N):
+        eval_Clenshaw(amat=a, x=x_, y=y_)
+    toc = timeit.default_timer()
+    time_Clenshaw = (toc-tic)/N*1e6
+    print(time_Clenshaw, 'us/call for Clenshaw implementation in 2D')
 
-assert(abs(eval_Clenshaw(amat=a, x=x_, y=y_) - eval_naive(amat=a, x=x_, y=y_)) < 1e-14)
+    assert(abs(eval_Clenshaw(amat=a, x=x_, y=y_) - eval_naive(amat=a, x=x_, y=y_)) < 1e-14)
 
-print(
-    eval_Clenshaw(amat=a, x=x_, y=y_), 
-    eval_naive(amat=a, x=x_, y=y_), 
-    f(x_, y_)
-)
+    print(
+        eval_Clenshaw(amat=a, x=x_, y=y_), 
+        eval_naive(amat=a, x=x_, y=y_), 
+        f(x_, y_)
+    )
+    o.append({
+        'M': M,
+        'naive': time_naive,
+        'Clenshaw': time_Clenshaw
+        })
+
+df = pandas.DataFrame(o)
+plt.plot(df['M'], df['naive'], 'o-')
+plt.plot(df['M'], df['Clenshaw'], 'o-')
+plt.yscale('log')
+plt.gca().set(xlabel='$M$, of $M^2$ matrix', ylabel='time / s')
+plt.show()
