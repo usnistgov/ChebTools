ad3ae3bbbb0ce94196eeaf607cc7951d9bcb3e8b
Ian Bell <ian.bell@nist.gov>
2024-01-29 11:56:29 -0500

    Fix dyadic_splitting (missing include)

--- src/nanobind_wrapper.cpp
+++ src/nanobind_wrapper.cpp
@@ -6,6 +6,7 @@
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/optional.h>
 #include <nanobind/stl/function.h>
+#include <nanobind/stl/vector.h>
 #include <nanobind/eigen/dense.h>
 
 #include "ChebToolsVersion.hpp"
